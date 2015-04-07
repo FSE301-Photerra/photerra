2ad9ce2944552e534c63cfdc8b7d344f036ebc97
Weston Neal <wtneal@asu.edu>
2015-04-07 13:04:57 -0700

    andrew's changes

--- check.php
+++ check.php
@@ -14,31 +14,33 @@ while($row = mysqli_fetch_array($result)) {
   
   if($row[1]==$username)
   {
-  	
-  	if($row[2]==$password)
-  	{
-  		//echo "right password";
-  		$validLogin=true;
-  		$member_id=$row[0];
-  		
-  	}
+    
+    if($row[2]==$password)
+    {
+      
+      $validLogin=true;
+      $member_id=$row[0];
+      
+    }
   }
  
 } 
-session_start();
+
 
 if($validLogin)
 {
-	$_SESSION['Logged_In']=1; 
-	$_SESSION["Id"]=$member_id; 
- 	header("Location: /userProfile.php?member_id=".$member_id);
- 	exit;
+  session_start();
+  $_SESSION['Logged_In']=1; 
+  $_SESSION["Id"]=$member_id; 
+  print_r($_SESSION);
+  header("Location: /userProfile.php?member_id=".$member_id);
+  exit;
 }
 else
-{	
-	$_SESSION["Logged_In"]=false; 
-	header("Location: /login.php?remarks=failure");
-	exit; 
+{ 
+  $_SESSION["Logged_In"]=false; 
+  header("Location: /login.php?remarks=failure");
+  exit; 
 }
 
 ?>
--- userProfile.php
+++ userProfile.php
@@ -1,28 +1,57 @@
+<html>
+<body>
+<head>
+    <title> Photerra </title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<nav class="top-bar" data-topbar>
+        <ul class="title-area">
+            <!-- Title Area -->
+            <li class="name">
+                <h1>
+                    <a href="" data-reveal-id="about-modal">
+                        PHOTERRA
+                    </a>
+                </h1>
+            </li>
+            <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
+        </ul>
+
+        <section class="top-bar-section">
+            <!-- Right Nav Section -->
+            <ul class="right">
+                <li class="divider"></li>
+                <li class="active">
+                    <a href="#" data-reveal-id="login-modal"><i class="user-icon"></i> Login</a>
+                </li>
+            </ul>
+        </section>
+    </nav>
+
+ <!--look at lines 104-149 of the picture -->
 <?php
-include 'app.php';
-include 'connect.php';
-
-try {
-    $member_id=$_GET['member_id'];
-
-    // Run the query and get the user details
-    $query="SELECT fname,lname,username FROM Users WHERE member_id='$member_id'";
-    $result=mysqli_query($link, $query) or die(mysqli_error($link));
-
-    // parse the query results
-    while($row = mysqli_fetch_assoc($result)) {
-        $fname=$row["fname"];
+ include 'connect.php';
+ $member_id=$_GET['member_id'];
+ $query="SELECT fname,lname,username FROM Users WHERE member_id='$member_id'";
+ $result=mysqli_query($link, $query) or die(mysqli_error($link));
+ while($row = mysqli_fetch_assoc($result)) {
+        
+    $fname=$row["fname"];
         $lname=$row["lname"];
         $username=$row["username"];
+
+        
     }
+   /*   
+   firstname = "<?php echo htmlspecialchars($fname); ?>"
+   lastname = "<?php echo htmlspecialchars($lname); ?>"
+   username = "<?php echo htmlspecialchars($username); ?>"
 
-    // render template
-    echo $twig->render('profile.twig', array(
-        'firstname' => $fname,
-        'lastname' => $lname,
-        'username' => $username,
-    ));
-
-} catch (Exception $e) {
-    die ('ERROR: ' . $e->getMessage());
-}
+    echo "hello $fname $lname";
+    echo ", your username is $username"; 
+    */
+ ?>
+<p><?php echo "Hello $fname $lname"; ?></p>
+<p><?php echo "Your username is $username"; ?></p>
+ </body>
+ </html>
